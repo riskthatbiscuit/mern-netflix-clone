5fcc537a47c01b9e22fedb624a8766f09a829f3c
Casey Mcgrath <caseymcgrath1000@gmail.com>
2024-07-31 17:51:33 -0400

    changing localhost target to 5000

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -3,15 +3,15 @@ const Footer = () => {
 		<footer className='py-6 md:px-8 md:py-0 bg-black text-white border-t border-gray-800'>
 			<div className='flex flex-col items-center justify-between gap-4 md:h-24 md:flex-row'>
 				<p className='text-balance text-center text-sm leading-loose text-muted-foreground md:text-left'>
-					{`Built by Risk That Bisucit (original source code from "As A Programmer" youtube channel)`}
+					{`Built by Risk That Bisucit (original source code from`}
 					<a
 						href='https://github.com/burakorkmez'
 						target='_blank'
 						className='font-medium underline underline-offset-4'
 					>
-						you
+						As A Programmer
 					</a>
-					. The source code is available on{" "}
+					 YouTube chanel. My enhanced code is available:{" "}
 					<a
 						href='https://github.com/riskthatbiscuit/mern-netflix-clone'
 						target='_blank'
